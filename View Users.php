ff58485dd4b9e7f9bd19ded230c238503244c698
manwar332 <55645709+manwar332@users.noreply.github.com>
2019-12-12 11:19:59 +0600

    Add files via upload

---
+++ View Users.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<table border="1">
+		<tr>
+		<th>ID</th>
+		<th>NAME</th>
+		<th>USER TYPE</th>
+	</tr>
+	<tr>
+		<td>
+			15-15-10101-1
+		</td>
+				<td>
+			BOb
+		</td>
+				<td>
+			Admin
+		</td>
+	</tr>
+	<tr>
+
+		<td>
+			16-10102-2
+		</td>
+				<td>
+			Anne
+		</td>
+				<td>
+			User
+		</td>
+	</tr>
+	<tr>
+
+
+		<td>
+			16-10103-2
+		</td>
+				<td>
+			Kent
+		</td>
+				<td>
+			User
+		</td>
+	</tr>
+	<tr>
+		<td>
+			16-10104-3
+		</td>
+				<td>
+			James
+		</td>
+				<td>
+			Admin
+		</td>
+
+
+	</tr>
+	</table>
+
+</body>
+</html>
